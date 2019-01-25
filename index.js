5a237696179fa12125a521b4ff0f4b8442ff9365
Christopher <christopher.carnivale@gmail.com>
2019-01-25 18:34:59 -0500

    refactored  -  replaced for loop, replaced if statement

--- index.js
+++ index.js
@@ -3,38 +3,39 @@
 'use strict';
 
 $('.js-dog-pictures-form').submit(function(event){
-  event.preventDefault();
+  event.preventDefault(); 
+  $('.js-dog-image-grid').empty(); 
   const inputValue = $('.js-number-input').val();
-
-  for (let i = 1; i <= inputValue; i++) {
-    console.log('For loop running'); 
-    fetch('https://dog.ceo/api/breeds/image/random')  
-      .then( response => response.json() )
-      .then( data => {
-        console.log(data);
-        console.log(data.message);
-        $('.js-dog-image-grid').prepend( `<img src="${data.message}" class="dog-picture" />` );
-      }); 
-  }
+  fetch(`https://dog.ceo/api/breeds/image/random/${inputValue}`) 
+    .then( response => response.json() )
+    .then( data => {
+      let html = data.message.map(item => `<img src= ${item}>`);  
+      html = html.join(' ');   
+      $('.js-dog-image-grid').html(html);  
+    }); 
 });
 // NEXT -- text block with server fail message - no breed of that name on server!
 // THEN -- text block with server success message - found the breed!
 $('.js-dog-breed-form').submit(function(event){
   event.preventDefault();
+  $('.js-dog-image-grid').empty(); 
   const breedValue = $('.js-breed-input').val();
-  console.log(breedValue);
   fetch(`https://dog.ceo/api/breed/${breedValue}/images/random`) 
-    .then( response => response.json() )
-    .then( data => {
-      console.log(data);
-      console.log(data.message);
-      if ( data.status === 'success' ) {
-        $('.breed-return-message').html(`<p>${breedValue} found!</p>`);
-        $('.js-dog-image-grid').prepend( `<img src="${data.message}" class="dog-picture" alt="image of a ${breedValue} dog breed"/>` );
+    .then(response => {
+      if (response.ok) { 
+        return response.json();   
       }
-      else if ( data.status === 'error' ) {
-        $('.breed-return-message').html(`<p>${data.message} :(</p>`);
-        $('.js-dog-image-grid').html();
-      }
+      console.log(response);
+      throw new Error(response.status);
+    }) 
+    .then(responseJson => {
+      $('.breed-return-message').html(`<p>${breedValue} found!</p>`);
+      $('.js-dog-image-grid').prepend( `<img src="${responseJson.message}" 
+      class="dog-picture" alt="image of a ${breedValue} dog breed"/>` );
+    })
+    
+    .catch(error => {
+      $('.breed-return-message').html(`<p>Something went wrong! 
+      ${error.message} Error</p>`);
     });
 });
